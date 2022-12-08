d36e7e2cea26b3f11cc51926d0cec87d5a443760
Ivan <popovitch.123@yandex.ru>
2022-12-08 20:01:49 +0300

    Добавил вывод результата из нейронки

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import { TestComponent } from './services/test/test.component';
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
   {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
-  { path: 'admin', loadChildren: () => import('./admin-panel/admin-panel.module').then(m => m.AdminPanelModule) },
-  { path: 'neuron', loadChildren: () => import('./neuron/neuron.module').then(m => m.NeuronModule) },
+  { path: 'admin', loadChildren: () => import('./admin-panel/admin-panel.module').then(m => m.AdminPanelModule), canActivate: [AuthGuard]},
+  { path: 'neuron', loadChildren: () => import('./neuron/neuron.module').then(m => m.NeuronModule), canActivate: [AuthGuard]},
   { path: 'test', component: TestComponent}
 
 ];
